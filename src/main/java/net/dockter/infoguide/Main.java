f6d6b148f4e906699218860ee47d647ede8675f4
Steven Downer <grinch@outlook.com>
2013-03-03 00:31:27 -0600

    Fix for player always equaling null & remove debug Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/net/dockter/infoguide/Main.java
+++ src/main/java/net/dockter/infoguide/Main.java
@@ -25,10 +25,6 @@
  */
 package net.dockter.infoguide;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.dockter.infoguide.gui.GUIGuide;
 import net.dockter.infoguide.guide.GuideManager;
 import org.bukkit.command.Command;
@@ -42,106 +38,123 @@ import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.keyboard.Keyboard;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main extends JavaPlugin {
 
-	private static Main instance;
-	private static FileConfiguration bypass;
-	public static String hotkeys = null;
+    private static Main instance;
+    private static FileConfiguration bypass;
+    public static String hotkeys = null;
 
-	public static Main getInstance() {
-		return instance;
-	}
+    public static Main getInstance() {
+        return instance;
+    }
 
-	@Override
-	public void onDisable() {
-		super.onDisable();
-		GuideManager.disable();
-	}
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        GuideManager.disable();
+    }
 
-	@Override
-	public void onEnable() {
-		super.onEnable();
-		instance = this;
-		FileConfiguration config = this.getConfig();
-		config.addDefault("PromptTitle", "Info Guide");
-		config.addDefault("TitleX", 190);
-		config.addDefault("DisplayOnLogin", true);
-		config.addDefault("Hot_Key", "KEY_F12");
-		config.addDefault("GUITexture", "http://www.almuramc.com/images/playerplus.png");
-		config.addDefault("DefaultGuide", "Initial");
-		config.addDefault("GuestGuide", "Initial");
-		config.addDefault("MemberGuide", "Initial");
-		config.addDefault("SuperMemberGuide", "Initial");
-		config.addDefault("ModeratorGuide", "Initial");
-		config.options().copyDefaults(true);
-		saveConfig();
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvents(new GuideListener(this), this);
-		File file = new File(this.getDataFolder() + File.separator + "users.yml");
-		if (!file.exists()) {
-			File init = new File(this.getDataFolder() + File.separator + "guides" + File.separator + "Initial.yml");
-			init.getParentFile().mkdirs();
-			try {
-				init.createNewFile();
-			} catch (IOException ex) {
-				Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-			}
-			YamlConfiguration yconf = YamlConfiguration.loadConfiguration(init);
-			yconf.addDefault("Name", "Initial");
-			yconf.addDefault("Date", "Future");
-			yconf.addDefault("Pages.Nr1", "Page 1 of InfoGuide");
-			yconf.options().copyDefaults(true);
-			try {
-				yconf.save(init);
-			} catch (IOException ex) {
-				Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-			}
-			try {
-				file.createNewFile();
-			} catch (IOException ex) {
-				Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-		bypass = YamlConfiguration.loadConfiguration(file);
-		hotkeys = config.getString("Hot_Key");
-		SpoutManager.getKeyBindingManager().registerBinding("InfoGuide", Keyboard.valueOf(Main.hotkeys), "Opens InfoGuide", new InfoGuideInputHandler(), Main.getInstance());
-	}
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        instance = this;
+        FileConfiguration config = this.getConfig();
+        config.addDefault("PromptTitle", "Info Guide");
+        config.addDefault("TitleX", 190);
+        config.addDefault("DisplayOnLogin", true);
+        config.addDefault("Hot_Key", "KEY_F12");
+        config.addDefault("GUITexture", "http://www.almuramc.com/images/playerplus.png");
+        config.addDefault("DefaultGuide", "Initial");
+        config.addDefault("GuestGuide", "Initial");
+        config.addDefault("MemberGuide", "Initial");
+        config.addDefault("SuperMemberGuide", "Initial");
+        config.addDefault("ModeratorGuide", "Initial");
+        config.options().copyDefaults(true);
+        saveConfig();
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(new GuideListener(this), this);
+        File file = new File(this.getDataFolder() + File.separator + "users.yml");
+        if (!file.exists()) {
+            File init = new File(this.getDataFolder() + File.separator + "guides" + File.separator + "Initial.yml");
+            init.getParentFile().mkdirs();
+            try {
+                init.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            YamlConfiguration yconf = YamlConfiguration.loadConfiguration(init);
+            yconf.addDefault("Name", "Initial");
+            yconf.addDefault("Date", "Future");
+            yconf.addDefault("Pages.Nr1", "Page 1 of InfoGuide");
+            yconf.options().copyDefaults(true);
+            try {
+                yconf.save(init);
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        bypass = YamlConfiguration.loadConfiguration(file);
+        hotkeys = config.getString("Hot_Key");
+        SpoutManager.getKeyBindingManager().registerBinding("InfoGuide", Keyboard.valueOf(Main.hotkeys), "Opens InfoGuide", new InfoGuideInputHandler(), Main.getInstance());
+    }
 
-	public static boolean isBypassing(String name) {
-		return bypass.contains(name) && bypass.getBoolean(name);
-	}
+    public static boolean isBypassing(String name) {
+        return bypass.contains(name) && bypass.getBoolean(name);
+    }
 
-	public static boolean canBypass(String name) {
-		return bypass.contains(name);
-	}
+    public static boolean canBypass(String name) {
+        return bypass.contains(name);
+    }
 
-	public void setBypass(String name, boolean value) {
-		bypass.set(name, value);
-		try {
-			bypass.save(new File(this.getDataFolder() + File.separator + "users.yml"));
-		} catch (IOException ex) {
-			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+    public void setBypass(String name, boolean value) {
+        bypass.set(name, value);
+        try {
+            bypass.save(new File(this.getDataFolder() + File.separator + "users.yml"));
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Player player = null;
-		if (cmd.getName().equalsIgnoreCase("gcme")) {
-			sender.sendMessage("InfoGuide - Secret GC Command.");
-			System.gc();
-		}
-		if (sender instanceof Player) {
-			player = (Player) sender;
-		}
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = null;
+        if (sender instanceof Player) {
+            player = (Player) sender;
+        }
+        if (cmd.getName().equalsIgnoreCase("infoguide")) {
+            if (player == null) {
+                sender.sendMessage("InfoGuide cannot be ran from the server console.");
+            } else {
+                if (args.length == 0) {
+                    ((SpoutPlayer) sender).getMainScreen().attachPopupScreen(new GUIGuide((SpoutPlayer) sender));
+                    return true;
+                } else if (args.length > 0) {
+                    String arg = new String();
 
-		if (cmd.getName().equalsIgnoreCase("infoguide")) {
-			if (player == null) {
-				sender.sendMessage("InfoGuide cannot be run from the server console.");
-			} else {
-				((SpoutPlayer) sender).getMainScreen().attachPopupScreen(new GUIGuide((SpoutPlayer) sender));
-			}
-			return true;
-		}
-		return false;
-	}
+                    for (int i = 0; i < args.length; i++) {
+                        arg += args[i] + " ";
+                    }
+                    if (GuideManager.getLoadedGuides().containsKey(arg.trim())) {
+                        GUIGuide guide = new GUIGuide((SpoutPlayer) sender);
+                        guide.setGuide(GuideManager.getLoadedGuides().get(arg.trim()));
+                        ((SpoutPlayer) sender).getMainScreen().attachPopupScreen(guide);
+                    } else {
+                        player.sendMessage("Unable to open guide, did you spell it correctly?");
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
