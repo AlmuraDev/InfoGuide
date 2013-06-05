c81d8fbcf116661d85f0c7adf4bc52f973846e1b
Steven Downer <grinch@outlook.com>
2013-06-05 14:34:16 -0500

    Cleanup - Fixed headers for all files - Remove unused imports - Dependency CraftBukkit changed to Bukkit - Will now shade metrics in (derp) - Added HEADER.txt - Updated LICENSE.txt - Removed .project, .classpath, .settings and infoguide.iml Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/net/dockter/infoguide/InfoGuideInputHandler.java
+++ src/main/java/net/dockter/infoguide/InfoGuideInputHandler.java
@@ -1,28 +1,21 @@
 /*
- * This file is part of Backpack.
+ * This file is part of InfoGuide.
  *
- * Copyright (c) 2012, AlmuraDev <http://www.almuramc.com/>
- * Backpack is licensed under the Almura Development License version 1.
+ * Copyright (c) 2012, AlmuraDev <http://www.almuradev.com/>
+ * InfoGuide is licensed under the Almura Development License.
  *
- * Backpack is free software: you can redistribute it and/or modify
+ * InfoGuide is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * As an exception, all classes which do not reference GPL licensed code
- * are hereby licensed under the GNU Lesser Public License, as described
- * in Almura Development License version 1.
- *
- * Backpack is distributed in the hope that it will be useful,
+ * InfoGuide is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License,
- * the GNU Lesser Public License (for classes that fulfill the exception)
- * and the Almura Development License version 1 along with this program. If not, see
- * <http://www.gnu.org/licenses/> for the GNU General Public License and
- * the GNU Lesser Public License.
+ * You should have received a copy of the GNU General Public License. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU General Public License.
  */
 package net.dockter.infoguide;
 
